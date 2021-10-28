5a1ca5f0fee6e229995e12209b2cb8831d07072a
Kerim Rahmanov <mrkrahmanov@gmail.com>
2021-10-28 14:14:46 +0500

    Optimized whole project, fixed image optimization, added better folder structure, added svg sprite creation

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,169 @@
-// DEPENDENCIES
-const { src, dest, watch, series } = require("gulp");
-const sourcemaps = require("gulp-sourcemaps");
-const terser = require("gulp-terser");
+const { src, dest, series, watch } = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
 const babel = require("gulp-babel");
-const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify-es").default;
+const del = require("del");
+const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass")(require("sass"));
-const prefix = require("gulp-autoprefixer");
-const minify = require("gulp-clean-css");
-const imagemin = require("gulp-imagemin");
+const svgSprite = require("gulp-svg-sprite");
+const fileInclude = require("gulp-file-include");
+const sourcemaps = require("gulp-sourcemaps");
+const htmlmin = require("gulp-htmlmin");
+const gulpif = require("gulp-if");
+const notify = require("gulp-notify");
+const image = require("gulp-image");
+const concat = require("gulp-concat");
 const imagewepb = require("gulp-webp");
 
-// JavaScript
-function minifyJS() {
-	return src("src/js/**/*.js")
-		.pipe(sourcemaps.init())
-		.pipe(babel({ presets: ["@babel/preset-env"] }))
-		.pipe(concat("main.min.js"))
-		.pipe(terser()) // { toplevel: true } - Optimizes variables
-		.pipe(sourcemaps.write("./"))
-		.pipe(dest("dist/assets/js"));
-}
+let isProd = false; // dev by default
 
-// Styles
-function compileStyles() {
-	return src(["src/scss/**/*.scss"])
-		.pipe(sass())
-		.pipe(prefix("last 3 versions"))
-		.pipe(minify())
-		.pipe(dest("dist/assets/css"));
-}
+const clean = () => {
+  return del(["app/*"]);
+};
 
-// Images
-function optimizeImage() {
-	return src("src/images/**/*.{jpg,png,svg}")
-		.pipe(
-			imagemin([
-				imagemin.mozjpeg({ quality: 80, progressive: true }),
-				imagemin.optipng({ optimizationLevel: 2 }),
-				imagemin.svgo({
-					plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
-				}),
-			])
-		)
-		.pipe(dest("dist/assets/images"));
-}
+// SVG sprite conversion
+const svgSprites = () => {
+  return src("./src/img/svg/**.svg")
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: "../sprite.svg", // sprite file name
+          },
+        },
+      })
+    )
+    .pipe(dest("./app/img"));
+};
+
+// STYLES
+const styles = () => {
+  return src("./src/scss/**/*.scss")
+    .pipe(gulpif(!isProd, sourcemaps.init()))
+    .pipe(sass().on("error", notify.onError()))
+    .pipe(autoprefixer("last 5 versions"))
+    .pipe(gulpif(isProd, cleanCSS({ level: 2 })))
+    .pipe(dest("./app/css/"))
+    .pipe(browserSync.stream());
+};
+
+// SCRIPTS
+const scripts = () => {
+  src("./src/js/vendor/**.js")
+    .pipe(concat("vendor.js"))
+    .pipe(gulpif(isProd, uglify().on("error", notify.onError())))
+    .pipe(dest("./app/js/"));
+  return src([
+    "./src/js/global.js",
+    "./src/js/components/**.js",
+    "./src/js/main.js",
+  ])
+    .pipe(gulpif(!isProd, sourcemaps.init()))
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
+    .pipe(concat("main.js"))
+    .pipe(gulpif(isProd, uglify().on("error", notify.onError())))
+    .pipe(gulpif(!isProd, sourcemaps.write(".")))
+    .pipe(dest("./app/js"))
+    .pipe(browserSync.stream());
+};
+
+// Other resources
+const resources = () => {
+  return src("./src/resources/**").pipe(dest("./app"));
+};
+
+// Image Optimization
+const images = () => {
+  return src([
+    "./src/img/**.jpg",
+    "./src/img/**.png",
+    "./src/img/**.jpeg",
+    "./src/img/*.svg",
+    "./src/img/**/*.jpg",
+    "./src/img/**/*.png",
+    "./src/img/**/*.jpeg",
+  ])
+    .pipe(gulpif(isProd, image()))
+    .pipe(dest("./app/img"));
+};
 
 // Convert Images
-function webpImage() {
-	return src("dist/assets/images/**/*.{jpg,png}")
-		.pipe(imagewepb())
-		.pipe(dest("dist/assets/images"));
-}
+const webpImage = () => {
+  return src("src/img/**/*.{jpg,jpeg,png}")
+    .pipe(imagewepb())
+    .pipe(dest("app/img/webp"));
+};
 
-// HTML
-function copyHTML() {
-	return src("src/*.html").pipe(dest("dist"));
-}
+const htmlInclude = () => {
+  return src(["./src/*.html"])
+    .pipe(
+      fileInclude({
+        prefix: "@",
+        basepath: "@file",
+      })
+    )
+    .pipe(dest("./app"))
+    .pipe(browserSync.stream());
+};
 
-// Watcher
-function watchTask() {
-	watch("src/scss/**/*.scss", compileStyles);
-	watch("src/js/**/*.js", minifyJS);
-	watch("src/images/**/*.{jpg,png,svg}", optimizeImage);
-	watch("dist/assets/images/**/*.{jpg,png}", webpImage);
-	watch("src/*.html", copyHTML);
-}
+const watchFiles = () => {
+  browserSync.init({
+    server: {
+      baseDir: "./app",
+    },
+  });
 
-// Default Gulp
+  watch("./src/scss/**/*.scss", styles);
+  watch("./src/js/**/*.js", scripts);
+  watch("./src/partials/*.html", htmlInclude);
+  watch("./src/*.html", htmlInclude);
+  watch("./src/resources/**", resources);
+  watch("./src/img/*.{jpg,jpeg,png,svg}", images);
+  watch("./src/img/**/*.{jpg,jpeg,png}", images);
+  watch("./src/img/svg/**.svg", svgSprites);
+};
+
+const htmlMinify = () => {
+  return src("app/**/*.html")
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(dest("app"));
+};
+
+const toProd = (done) => {
+  isProd = true;
+  done();
+};
+
 exports.default = series(
-	compileStyles,
-	minifyJS,
-	optimizeImage,
-	webpImage,
-	copyHTML,
-	watchTask
+  clean,
+  htmlInclude,
+  scripts,
+  styles,
+  resources,
+  images,
+  webpImage,
+  svgSprites,
+  watchFiles
+);
+
+exports.build = series(
+  toProd,
+  clean,
+  htmlInclude,
+  scripts,
+  styles,
+  resources,
+  images,
+  webpImage,
+  svgSprites,
+  htmlMinify
 );
--- src/js/02_script.js
+++
@@ -1,1 +0,0 @@
-console.log("yayy it's working");
